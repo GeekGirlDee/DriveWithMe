82fcdaa086c3c60893c4f244692431cb644485f1
GeekGirlDee <dmabitle839@gmail.com>
2018-09-10 15:03:16 +0200

    Drive With Me

--- app/src/main/java/com/example/android/drivewithme/MainActivity.java
+++ app/src/main/java/com/example/android/drivewithme/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.android.drivewithme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button TnC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TnC = (Button) findViewById(R.id.tc);
+        TnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeScreen();
+            }
+        });
+    }
+
+    public void HomeScreen()
+    {
+        Intent intent = new Intent(this, HomeScreen.class);
+        startActivity(intent);
     }
 }
